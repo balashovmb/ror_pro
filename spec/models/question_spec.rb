64d19d0e40d61c62c8236afcd1b8d940fe88eb95
balashovmb <balashovmb@gmail.com>
2017-03-08 00:14:30 +0500

    Lesson3 fixes based on comments

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should validate_length_of(:title).is_at_least(10) }
-  it { should validate_length_of(:body).is_at_least(10) }
+  it { should validate_length_of(:title).is_at_least(10).is_at_most(255) }
   it { should have_many(:answers) }  
 end
 
