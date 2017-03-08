53a139192811b3ac0a41a42a6c4aaeb055ea260c
balashovmb <balashovmb@gmail.com>
2017-03-08 12:55:50 +0500

    Fix in Question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_length_of(:title).is_at_least(10) }
+  it { should validate_length_of(:body).is_at_least(10) }
   it { should validate_length_of(:title).is_at_least(10).is_at_most(255) }
-  it { should have_many(:answers) }  
+  it { should have_many(:answers).dependent(:destroy) }  
 end
 
