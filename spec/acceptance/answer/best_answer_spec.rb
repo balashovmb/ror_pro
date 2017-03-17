a1e879ba914be56a265a6c5e5b14286ad0203160
balashovmb <balashovmb@gmail.com>
2017-03-17 21:46:41 +0500

    best answer start

---
+++ spec/acceptance/answer/best_answer_spec.rb
@@ -0,0 +1,26 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Best answer', %q{
+  Author of question 
+  Can mark answer as best
+} do
+
+  given!(:question) {create(:question) }
+  given!(:answer) { create(:answer, question: question)}
+
+
+  context 'Authenticated user' do
+    scenario 'user is author of the question', js: true do
+      sign_in(question.user)
+      visit question_path(question)
+
+      save_and_open_page
+      click_on 'Set best'
+
+      within '.best' do
+        expect(page).to have_content 'Best answer!' 
+      end 
+    end
+  end
+end
+
