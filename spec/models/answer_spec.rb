19c592c2f67be61665a45beddf310e6050e24a18
balashovmb <balashovmb@gmail.com>
2017-03-23 16:36:12 +0500

    Polymorphyc attachments

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,13 +4,15 @@ RSpec.describe Answer, type: :model do
   it { should validate_length_of(:body).is_at_least(10) }
   it { should belong_to(:question) }
   it { should belong_to(:user) }
-
-  let(:question) { create(:question) }
-  let(:answer1) { create(:answer, question: question) }
-  let(:answer2) { create(:answer, question: question, best: false ) }
-
+  it { should have_many(:attachments).dependent(:destroy)}  
+  it { should accept_nested_attributes_for :attachments }  
 
   context 'set_best method' do
+
+    let(:question) { create(:question) }
+    let(:answer1) { create(:answer, question: question) }
+    let(:answer2) { create(:answer, question: question, best: false ) }    
+
     it 'changes attribute best to true' do
       answer1.set_best
       expect(answer1.best).to eq true
