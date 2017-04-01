6dd54c8ccf22c7426c93e20e8944fef5ea6d2165
balashovmb <balashovmb@gmail.com>
2017-04-01 20:00:22 +0500

    Vote up acceptance tests

--- spec/acceptance/question/votes_question_spec.rb
+++ spec/acceptance/question/votes_question_spec.rb
@@ -4,13 +4,40 @@ feature 'Vote for question', %q{
   User can vote to increase or decrease
   another user's question rating  
 } do
-  given!(:user) { create(:user) }
+  given(:user) { create(:user) }
   given(:question) { create(:question) }
+  given(:users_question) { create(:question, user: user) }
 
-  scenario "authenticated user can vote to increase question's rating" do
-    sign_in(user)
-    visit question_path(question)
+  context 'Vote up' do
 
-    expect(page).to have_content '+'
+    scenario "authenticated user can vote to increase question's rating", js: true do
+      sign_in(user)
+      visit question_path(question)
+      
+      within ".question" do
+        click_link '+'
+        expect(page).to have_content 'Rating: 1'      
+      end
+    end
+
+    scenario "author of the question don't see links for vote", js: true do
+      sign_in(user)
+      visit question_path(users_question)
+      
+      within ".question" do
+        expect(page).not_to have_content '+'  
+      end
+    end
+
+    scenario "authenticated user can't vote twice", js: true do
+      sign_in(user)
+      visit question_path(question)
+      
+      within ".question" do
+        click_link '+'
+        click_link '+'      
+        expect(page).to have_content 'Rating: 1'      
+      end
+    end
   end
 end
