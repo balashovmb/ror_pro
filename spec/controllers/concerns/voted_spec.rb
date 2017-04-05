8881c74d95ed9f9b0bef937b2477ace0010a844c
balashovmb <balashovmb@gmail.com>
2017-04-05 14:53:16 +0500

    Little fix of spec

--- spec/controllers/concerns/voted_spec.rb
+++ spec/controllers/concerns/voted_spec.rb
@@ -9,41 +9,41 @@ RSpec.shared_examples 'voted' do
     let(:users_votable) { create(votable_klass, user: @user) }
 
     context "Vote up for another user's question/answer" do
-      it "assigns the answer/post to @votable" do
+      it "assigns the answer/question to @votable" do
         post :vote_up, params: { id: votable.id }
         expect(assigns(:votable)).to eq votable
       end
 
       it "change question/answer rating to 1" do
-        expect { post :vote_up, params: { id: votable.id }, format: :js }.to change { votable.reload.rating }.by(1)
+        expect { post :vote_up, params: { id: votable.id }, format: :json }.to change { votable.reload.rating }.by(1)
       end
     end
 
     context "Vote down for another user's question/answer" do
-      it "assigns the answer/post to @votable" do
+      it "assigns the answer/question to @votable" do
         post :vote_down, params: { id: votable.id }
         expect(assigns(:votable)).to eq votable
       end
 
       it "change question/answer rating to -1" do
-        expect { post :vote_down, params: { id: votable.id }, format: :js }.to change { votable.reload.rating }.by(-1)
+        expect { post :vote_down, params: { id: votable.id }, format: :json }.to change { votable.reload.rating }.by(-1)
       end
     end
 
     context "User is author of question" do
       it "vote up don't changes rating" do
-        expect { post :vote_up, params: { id: users_votable.id }, format: :js }.not_to change { votable.reload.rating }
+        expect { post :vote_up, params: { id: users_votable.id }, format: :json }.not_to change { votable.reload.rating }
       end
 
       it "vote down don't changes rating" do
-        expect { post :vote_down, params: { id: users_votable.id }, format: :js }.not_to change { votable.reload.rating }
+        expect { post :vote_down, params: { id: users_votable.id }, format: :json }.not_to change { votable.reload.rating }
       end
     end
 
     context "Cancel vote" do
       it "deletes vote" do
-        post :vote_up, params: { id: votable.id }, format: :js
-        delete :cancel_vote, params: { id: votable.id }, format: :js
+        post :vote_up, params: { id: votable.id }, format: :json
+        delete :cancel_vote, params: { id: votable.id }, format: :json
         expect(votable.reload.rating).to eq 0
       end
     end
