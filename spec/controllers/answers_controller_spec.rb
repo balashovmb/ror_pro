19f619e1d15c25afce14b858bd1301cc09ba1de5
balashovmb <balashovmb@gmail.com>
2017-04-05 15:59:25 +0500

    Voted spec fixes

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   it_should_behave_like 'voted'
-    
+
   let!(:question) { create(:question) }
   let!(:user) { question.user }
   let(:answer) { create(:answer, user: user, question: question) }
--- spec/controllers/concerns/voted_spec.rb
+++ spec/controllers/concerns/voted_spec.rb
@@ -1,51 +1,78 @@
 require 'rails_helper'
 
 RSpec.shared_examples 'voted' do
+  votable_klass = described_class.controller_name.singularize.to_sym
+  let!(:votable) { create(votable_klass) }
+  let(:users_votable) { create(votable_klass, user: @user) }
+  sign_in_user
+
   describe 'POST #vote_up' do
+    subject(:vote_up) { post :vote_up, params: { id: votable.id }, format: :json }
     sign_in_user
 
-    votable_klass = described_class.controller_name.singularize.to_sym
-    let!(:votable) { create(votable_klass) }
-    let(:users_votable) { create(votable_klass, user: @user) }
-
-    context "Vote up for another user's question/answer" do
-      it "assigns the answer/question to @votable" do
-        post :vote_up, params: { id: votable.id }
-        expect(assigns(:votable)).to eq votable
-      end
-
-      it "change question/answer rating to 1" do
-        expect { post :vote_up, params: { id: votable.id }, format: :json }.to change { votable.reload.rating }.by(1)
-      end
+    it "assigns the answer/question to @votable" do
+      vote_up
+      expect(assigns(:votable)).to eq votable
     end
 
+    it "change question/answer rating to 1" do
+      expect { vote_up }.to change { votable.reload.rating }.by(1)
+    end
+
+    it "renders json" do
+      vote_up
+      expect(response.headers['Content-Type']).to match /json/
+    end
+
+    context "User is author of question" do
+      subject(:vote_up_own_voteble) { post :vote_up, params: { id: users_votable.id }, format: :json }
+      it "vote up don't changes rating" do
+        expect { vote_up_own_voteble }.not_to change { votable.reload.rating }
+      end
+      it "renders json" do
+        vote_up_own_voteble
+        expect(response.headers['Content-Type']).to match /json/
+      end
+    end
+  end
+  describe 'POST #vote_down' do
+    sign_in_user
+
+    subject(:vote_down) { post :vote_down, params: { id: votable.id }, format: :json }
     context "Vote down for another user's question/answer" do
       it "assigns the answer/question to @votable" do
-        post :vote_down, params: { id: votable.id }
+        vote_down
         expect(assigns(:votable)).to eq votable
       end
 
       it "change question/answer rating to -1" do
-        expect { post :vote_down, params: { id: votable.id }, format: :json }.to change { votable.reload.rating }.by(-1)
+        expect { vote_down }.to change { votable.reload.rating }.by(-1)
       end
     end
 
     context "User is author of question" do
-      it "vote up don't changes rating" do
-        expect { post :vote_up, params: { id: users_votable.id }, format: :json }.not_to change { votable.reload.rating }
-      end
-
+      subject(:vote_down_own_voteble) { post :vote_down, params: { id: users_votable.id }, format: :json }
       it "vote down don't changes rating" do
-        expect { post :vote_down, params: { id: users_votable.id }, format: :json }.not_to change { votable.reload.rating }
+        expect { vote_down_own_voteble }.not_to change { votable.reload.rating }
       end
     end
+  end
 
-    context "Cancel vote" do
-      it "deletes vote" do
-        post :vote_up, params: { id: votable.id }, format: :json
-        delete :cancel_vote, params: { id: votable.id }, format: :json
-        expect(votable.reload.rating).to eq 0
-      end
+  describe 'DELETE #cancel_vote' do
+    subject(:vote_and_cancel) do
+      post :vote_up, params: { id: votable.id }, format: :json
+      delete :cancel_vote, params: { id: votable.id }, format: :json
+    end
+    sign_in_user
+
+    it "deletes vote" do
+      vote_and_cancel
+      expect(votable.reload.rating).to eq 0
+    end
+
+    it "renders json" do
+      vote_and_cancel
+      expect(response.headers['Content-Type']).to match /json/
     end
   end
 end
