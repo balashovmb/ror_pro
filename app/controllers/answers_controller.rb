694ea0d50a4f623dd30ef85902ec3930d83c909c
balashovmb <balashovmb@gmail.com>
2017-04-10 15:26:00 +0500

    Gon user_id added

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -39,7 +39,12 @@ class AnswersController < ApplicationController
 
   def publish_answer
     return if @answer.errors.any?
-    ActionCable.server.broadcast "question_answers_#{@question.id}", @answer
+    ActionCable.server.broadcast(
+      "question_answers_#{@question.id}",
+      answer: @answer,
+      question_author_id: @question.user_id
+    )
+
   end   
 
   def set_question
