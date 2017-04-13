98d43f287255a01b9e7764129cce9fbd1d4e5c2a
balashovmb <balashovmb@gmail.com>
2017-04-13 10:13:02 +0500

    Vote answers spec multiple sessions done

--- spec/acceptance/answer/votes_answer_spec.rb
+++ spec/acceptance/answer/votes_answer_spec.rb
@@ -6,8 +6,8 @@ feature 'Vote for answer', %q{
 } do
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
-  given(:answer) { create(:answer, question: question) }
-  given(:users_answer) { create(:answer, question: question, user: user) }
+  given!(:answer) { create(:answer, question: question) }
+  given!(:users_answer) { create(:answer, question: question, user: user) }
   given(:user2) { create(:user) }
 
   before do
@@ -99,7 +99,7 @@ feature 'Vote for answer', %q{
 
     scenario "answer Vote UP link works in another users session", js: true do
       Capybara.using_session('user2') do
-        within '.answers' do
+        within '#answer-3' do
 
           click_link 'Vote UP'
           expect(page).to have_content 'Rating: 1'
@@ -109,7 +109,7 @@ feature 'Vote for answer', %q{
 
     scenario "answer Vote DOWN link works in another users session", js: true do
       Capybara.using_session('user2') do
-        within '.answers' do
+        within '#answer-3' do
 
           click_link 'Vote DOWN'
           expect(page).to have_content 'Rating: -1'
@@ -119,7 +119,7 @@ feature 'Vote for answer', %q{
 
     scenario "answer Cancel vote link works in another users session", js: true do
       Capybara.using_session('user2') do
-        within '.answers' do
+        within '#answer-3' do
           click_link 'Vote DOWN'
           click_link 'Cancel'
           expect(page).to have_content 'Rating: 0'                    
