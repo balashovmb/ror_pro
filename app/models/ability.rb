87f9ac69d363ce6e1102a1c91265141eb7671bc9
balashovmb <balashovmb@gmail.com>
2017-04-29 12:57:26 +0500

    Ability spec green

---
+++ app/models/ability.rb
@@ -0,0 +1,30 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    alias_action :vote_up, :vote_down, :cancel_del, to: :vote
+
+    @user = user
+    if @user
+      user_abilities
+    else
+      guest_abilities
+    end
+  end
+
+  private
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def user_abilities
+    guest_abilities 
+    can :create, [Question, Answer, Comment, Attachment]
+    can [:update, :destroy], [Question, Answer, Comment], user_id: @user.id
+    can :destroy, Attachment, attachable: { user_id: @user.id }
+    can :set_best, [Answer] { |answer| @user.author?(answer.question) }
+    can :vote, [Question, Answer] { |votable| !@user.author?(votable) }
+  end
+end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Ability, type: :model do
+  subject(:ability) { Ability.new(user) }
+
+  context 'when user guest' do
+    let(:user) { nil }
+    it { should be_able_to :read, :all }
+    it { should_not be_able_to :manage, :all }
+  end
+
+  context 'when user authorized' do
+    let(:user) { create :user }
+    let(:another_user) { create :user }
+
+    let(:question) { create :question, user: user }
+    let(:question2) { create :question, user: another_user }
+
+    it { should be_able_to :read, :all }
+
+    it { should be_able_to :create, Question }
+    it { should be_able_to :create, Answer }
+    it { should be_able_to :create, Comment }
+    it { should be_able_to :create, Attachment }
+
+    it { should be_able_to :destroy, create(:question, user: user) }
+    it { should be_able_to :destroy, create(:answer, user: user) }
+    it { should be_able_to :destroy, create(:attachment, attachable: question) }
+    it { should be_able_to :destroy, create(:comment, user: user) }
+    it { should_not be_able_to :destroy, create(:question, user: another_user) }
+    it { should_not be_able_to :destroy, create(:answer, user: another_user) }
+    it { should_not be_able_to :destroy, create(:attachment, attachable: question2) }
+    it { should_not be_able_to :destroy, create(:comment, user: another_user) }
+
+    it { should be_able_to :update, create(:question, user: user) }
+    it { should be_able_to :update, create(:answer, user: user) }
+    it { should_not be_able_to :update, create(:question, user: another_user) }
+    it { should_not be_able_to :update, create(:answer, user: another_user) }
+
+    it { should be_able_to :vote, create(:question, user: another_user) }
+    it { should be_able_to :vote, create(:answer, user: another_user) }
+    it { should_not be_able_to :vote, create(:question, user: user) }
+    it { should_not be_able_to :vote, create(:answer, user: user) }
+
+    it { should be_able_to :set_best, create(:answer, question: question, user: user) }
+    it { should_not be_able_to :set_best, another_user}
+  end
+end 
