0f3de43ef9cebadbd75be220c4a7268d34056aef
balashovmb <balashovmb@gmail.com>
2017-05-18 15:49:56 +0500

    Acceptance

---
+++ spec/acceptance/search/search_spec.rb
@@ -0,0 +1,74 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Search', %q{
+  To be able to find information, anyone can use search
+} do
+
+  let!(:question1) { create(:question, title: 'Founded question 1')}
+  let!(:question2) { create(:question, title: 'Founded question 2')}
+  let!(:answer) { create(:answer, body: 'Founded answer')}
+  let!(:comment1) { create(:comment, commentable: question1, body: 'Founded question comment')}
+  let!(:comment2) { create(:comment, commentable: answer, body: 'Founded answer comment')}
+  let!(:user) { create(:user, email: 'founded@test.com')}
+
+  let!(:not_findable) { create(:question, title: 'not_findable')}
+
+  before do
+    index
+    visit root_path
+  end
+
+  scenario 'Empty query', js: true do
+    click_button 'Find'
+
+    expect(page).to have_content('No results')
+
+  end
+
+  scenario 'Search all contexts', js: true do
+    fill_in 'query', with: 'Founded'
+    click_button 'Find'
+
+    expect(page).to have_content('Founded question 1')
+    expect(page).to have_content('Founded question 2')
+    expect(page).to have_content('Founded answer')
+    expect(page).to have_content('Founded question comment')
+    expect(page).to have_content('Founded answer comment')
+    expect(page).to have_content('founded@test.com')
+    expect(page).not_to have_content('not_findable')
+  end
+
+  scenario 'User searches questions', js: true do
+    fill_in 'query', with: 'Founded'
+    select 'Questions', from: 'object'
+    click_button 'Find'
+
+    expect(page).to have_content('Founded question 1')
+    expect(page).to have_content('Founded question 2')
+  end
+
+  scenario 'User searches answers', js: true do
+    fill_in 'query', with: 'Founded'
+    select 'Answers', from: 'object'
+    click_button 'Find'
+
+    expect(page).to have_content('Founded answer')
+  end
+
+  scenario 'User searches comments', js: true do
+    fill_in 'query', with: 'Founded'
+    select 'Comments', from: 'object'
+    click_button 'Find'
+
+    expect(page).to have_content('Founded answer comment')
+    expect(page).to have_content('Founded question comment')
+  end
+
+  scenario 'User searches users', js: true do
+    fill_in 'query', with: 'Founded'
+    select 'Users', from: 'object'
+    click_button 'Find'
+
+    expect(page).to have_content('founded@test.com')
+  end
+end
