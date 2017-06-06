09e132446ddb0a9e35a4fd3cc2b150ac644da42a
balashovmb <balashovmb@gmail.com>
2017-06-06 15:35:18 +0500

    sendgrid2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: '51.15.67.2' }
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address: '127.0.0.1',
   #   port: 25,
